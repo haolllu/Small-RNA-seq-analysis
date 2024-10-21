9cdc8e441f1db8b6f58cafa5e336b02db23f3f35
RooKo <Haolu24@outlook.com>
2024-10-21 15:20:03 +0900

    Update 1_fastqc.sh

--- Shell/1_fastqc/1_fastqc.sh
+++ Shell/1_fastqc/1_fastqc.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-#SBATCH -o /home/Sakai-lab/hao.lu.gv/workspace/sperm_cohort7/1_fastqc/1_fastqc.out
-#SBATCH -e /home/Sakai-lab/hao.lu.gv/workspace/sperm_cohort7/1_fastqc/1_fastqc.err
+#These are the commands for configuring SLURM job parameters.
+#Please adjust or remove them based on your actual runtime environment.
+#SBATCH -o /to_your_directory/1_fastqc/1_fastqc.out
+#SBATCH -e /to_your_directory/1_fastqc/1_fastqc.err
 #SBATCH -c 16
 #SBATCH --mem-per-cpu=4G
 
-fastqc /home/Sakai-lab/hao.lu.gv/workspace/sperm_cohort7/0_raw_data/*T_1.fq \
-	--nogroup -o /home/Sakai-lab/hao.lu.gv/workspace/sperm_cohort7/1_fastqc
+#Run fastqc on the raw sequencing data;
+#We are only using the forward reads from the paired-end sequencing data.
+fastqc /to_your_directory/0_raw_data/*_1.fq \
+	--nogroup -o /to_your_directory/1_fastqc
