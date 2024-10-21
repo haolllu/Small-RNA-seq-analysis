1e783e79d645127f5be159f41093c09634d9de2f
RooKo <Haolu24@outlook.com>
2024-10-21 15:27:50 +0900

    Update 3_2nd_fastqc.sh

--- Shell/3_2nd_fastqc/3_2nd_fastqc.sh
+++ Shell/3_2nd_fastqc/3_2nd_fastqc.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-#SBATCH -o /home/Sakai-lab/hao.lu.gv/workspace/sperm_cohort7/3_2nd_fastqc/3_2nd_fastqc.out
-#SBATCH -e /home/Sakai-lab/hao.lu.gv/workspace/sperm_cohort7/3_2nd_fastqc/3_2nd_fastqc.err
+#These are the commands for configuring SLURM job parameters.
+#Please adjust or remove them based on your actual runtime environment.
+#SBATCH -o /to_your_directory/3_2nd_fastqc/3_2nd_fastqc.out
+#SBATCH -e /to_your_directory/3_2nd_fastqc/3_2nd_fastqc.err
 #SBATCH -c 8
 #SBATCH --mem-per-cpu=16G
 
-fastqc /home/Sakai-lab/hao.lu.gv/workspace/sperm_cohort7/0_fastp_data/*.fq \
-	--nogroup -o /home/Sakai-lab/hao.lu.gv/workspace/sperm_cohort7/3_2nd_fastqc -t 16
+#Run fastqc on the trimmed forward sequencing data.
+fastqc /to_your_directory/0_fastp_data/*.fq \
+	--nogroup -o /to_your_directory/3_2nd_fastqc -t 16
